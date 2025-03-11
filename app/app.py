3b8eeb3a7918404a69164a58d47147036efc10b6
karim <email@example.com>
2025-03-11 11:41:34 +0000

    Push for demo

--- app/app.py
+++ app/app.py
@@ -10,7 +10,7 @@ st.set_page_config(
 )
 
 # App header
-st.title("🚀 Streamlit App on AWS EKS Demo with CI/CD v2")
+st.title("🚀 Streamlit App on AWS EKS Demo from Safran premises")
 st.markdown("### Deployed with Kaniko on Amazon EKS")
 
 # Sidebar
