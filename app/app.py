e55c736975e2bde0f522dcd9bc6db26a0d32d5bc
The Mirak <k.benzidane@gmail.com>
2025-07-03 14:42:01 +0200

    Commit from Safran

--- app/app.py
+++ app/app.py
@@ -10,7 +10,7 @@ st.set_page_config(
 )
 
 # App header
-st.title("🚀 Streamlit App on AWS EKS Demo ... from CA !!!!")
+st.title("🚀 Streamlit App on AWS EKS Demo ... from Safran !!!!")
 st.markdown("### Deployed with Kaniko on Amazon EKS")
 
 # Sidebar
